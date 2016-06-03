d54f31a8e18495163d2d7757693ad215e4e27514
mehreencs87 <mehreen.cs87@gmail.com>
2016-06-03 15:58:38 +0500

    seed

---
+++ 00-Starter-Seed/Modules/Authentication.cs
@@ -0,0 +1,29 @@
+﻿using Auth0.Nancy.SelfHost;
+using Nancy;
+using System.Configuration;
+
+namespace auth0_nancyfx_sample
+{
+    public class Authentication
+        : NancyModule
+    {
+        public Authentication()
+        {
+            Get["/login"] = o =>
+            {
+                if (this.SessionIsAuthenticated())
+                    return Response.AsRedirect("securepage");
+
+                return View["login", new { client_id = ConfigurationManager.AppSettings["auth0:ClientId"], domain = ConfigurationManager.AppSettings["auth0:domain"] }];
+            };
+
+            Get["/login-callback"] = o => this
+                .AuthenticateThisSession()
+                .ThenRedirectTo("securepage");
+
+            Get["/logout"] = o => this
+                .RemoveAuthenticationFromThisSession()
+                .ThenRedirectTo("index");
+        }
+    }
+}
